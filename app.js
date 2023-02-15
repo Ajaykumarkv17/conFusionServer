06f6c2043db436c2fdbd7de7698ebaf87b7fe5b2
Ajay Kumar <ajaykumarkv0317@gmail.com>
2023-02-16 00:06:37 +0530

    cookies

--- app.js
+++ app.js
@@ -31,33 +31,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09878-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
+ 
+  if(!req.signedCookies.user){
+      var authHeader=req.headers.authorization;
+      if(!authHeader){
+        var err=new Error('Invalid authorization headerr');
 
-  var authHeader=req.headers.authorization;
-  if(!authHeader){
-    var err=new Error('Invalid authorization headerr');
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status=401;
+        return next(err);
+      }
+      var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+      var username=auth[0];
+      var password=auth[1];
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
-  }
-  var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-  var username=auth[0];
-  var password=auth[1];
+      if(username=='admin'&&password=='ajay2003'){
+        res.cookie('user','admin',{signed:true})// authorized
+        next();
+      }
+      else{
+        var err=new Error('Invalid authorization headerr');
 
-  if(username=='admin'&&password=='ajay2003'){
-    next();
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status=401;
+        return next(err);
+      }
   }
   else{
-    var err=new Error('Invalid authorization headerr');
+    if(req.signedCookies.user==='admin'){
+      next();
+    }
+    else{
+       var err=new Error('Invalid authorization headerr');
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+       
+        err.status=401;
+        return next(err);
+    }
   }
+  
 }
 
 //authorization
